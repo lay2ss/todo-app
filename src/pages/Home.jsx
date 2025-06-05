154587577f44b8be4fffff8a5d915230ecc755b4
lay2ss <lais222lai@gmail.com>
2025-06-04 22:32:02 -0300

    fix layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,12 +21,24 @@ const Home = () => {
     }, [])
 
     return (
-        <main className="m-auto flex flex-col items-center border-amber-500 border-4">
+        <main className="bg-img m-auto flex flex-col items-center w-min">
             <div className="bg-section">
                 <div className="flex justify-between px-5"><h1 className=" py-10 text-xl tracking-[10px] ">TODO</h1>
+                
                 <div className="h-min my-10"><img className="" src="src/assets/images/icon-moon.svg" alt="moon icon" /></div>
                 </div>
-                <input className= "mx-auto h-10 w-9/10 bg-white rounded-sm" type="text" />
+                <div className="grid gap-5 justify-center ">
+                <div className= "text-black px-5 mx-auto h-10 w-90 bg-white rounded-sm sm:w-110 flex">
+                    <div className="flex items-center">
+                    <input className="appearance-none w-5 h-5 border-gray-400 border-2 rounded-xl checked:bg-gradient-to-br checked:from-[#57ddff] checked:to-[#c058f3] checked:border-0" type="checkbox" id="checkbox" />
+                    </div>
+                    <div className="flex items-center">
+                    <input className="px-3 h-min" type="text" placeholder="Create a new todo..." />
+                    </div>
+                </div>
+                <div className="mx-auto h-80 w-90 bg-white rounded-sm sm:w-110 sm:h-90"></div>
+                <div className="mx-auto w-90 h-10 bg-white rounded-sm sm:hidden "></div>
+                </div>
             </div>
             <button onClick={handleDarkBg} className={`${mode === "false"? "text-white" : "text-black"}`}>dark</button>
         </main>
