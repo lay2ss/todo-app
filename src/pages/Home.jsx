3eecc06d602a824bd4a26e08dc9214b647b2d94f
lay2ss <lais222lai@gmail.com>
2025-06-07 15:25:36 -0300

    apply Add and Delete features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
 
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef } from "react";
 
 const Home = () => {
     let [ isDarkMode, setIsDarkMode ] = useState("false" || mode);
     let [icon, setIcon ] = useState(isDarkMode === "false"? "src/assets/images/icon-moon.svg" : "src/assets/images/icon-sun.svg");
+    const [task, setTask] = useState('');
+    let [addTask, setAddTask] = useState([]);
+
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.key === 'Enter') {
+            if(task.length !== 0 && task.trim(" ")){
+                setAddTask([...addTask, task]);
+                setTask('');
+            } 
+        }
+    }
+    const deleteTask = (index) => {
+        setAddTask(addTask.filter((_, i) => i !== index));
+    }
+
     const handleDarkBg = () => {
         isDarkMode === "false"? setIsDarkMode("true") : setIsDarkMode("false") && document.body.classList.remove('dark-bg');
         document.body.classList.add('dark-bg');
@@ -39,18 +58,32 @@ const Home = () => {
                 <div className="grid gap-5 justify-center">
                     <div className={`px-5 mx-auto h-10 w-90 bg-white rounded-sm sm:w-150 flex ${mode === "false"? "dark-card" : ""}`}>
                         <div className="flex items-center">
-                        <input className="hover-effect appearance-none w-5 h-5 border-gray-400 border-2 rounded-xl checked-state" type="checkbox" id="checkbox" />
+                        <input className="hover-effect appearance-none w-5 h-5 border-gray-400 border-2 rounded-xl checked-state" type="checkbox" />
                         </div>
                         <div className="flex items-center">
-                        <input className={`px-3 h-5 pt-1 ${mode === "false"? "placeholder:text-white opacity-50" : "placeholder:text-gray-400"}`} type="text" placeholder="Create a new todo..." /> 
+                        <input className={`px-3 h-5 pt-1 ${mode === "false"? "placeholder:text-white opacity-50 text-white" : "placeholder:text-gray-400 text-gray-400"}`} type="text" onKeyDownCapture={handleKeyDown} value={task} onChange={handleChange} placeholder="Create a new todo..." /> 
                         </div>
                     </div>
-                    <div className={`flex mx-auto h-80 w-90 bg-white rounded-sm sm:w-150 sm:h-100 ${mode === "false"? "dark-card" : ""}`}>
-                        <div className="sm:hidden text-gray-400 grid grid-cols-2 self-end w-90 opacity-60 h-10">
+                    <div className={`flex flex-col mx-auto h-80 w-90 bg-white rounded-sm sm:w-150 sm:h-100 overflow-y-auto overflow-x-hidden ${mode === "false"? "dark-card" : ""}`}> 
+                        <div>{addTask.map((task, index ) => 
+                            (<div key={index} className={`border-1 px-5 mx-auto h-10 w-90 bg-white sm:w-150 flex ${mode === "false"? "dark-card border-gray-600" : "border-gray-200 border-r-transparent border-l-transparent"}`}>
+                                <div className="flex items-center">
+                                    <input className="hover-effect appearance-none w-5 h-5 border-gray-400 border-2 rounded-xl checked-state" type="checkbox" />
+                                </div>
+                                <div className="flex items-center w-90 sm:w-150 justify-between">
+                                    <p className={`px-3 h-5 ${mode === "false"? "opacity-50 text-white" : "text-gray-400"}`}>{task} </p>             
+                                    <img onClick={() => deleteTask(index)} src="src/assets/images/icon-cross.svg" alt="cross icon" />               
+                                </div>
+                            </div>)
+                            )}      
+                        </div>
+                        <div className="text-gray-400 flex">
+                        </div>
+                        <div className={`border-2 sm:hidden text-gray-400 grid grid-cols-2 mt-auto w-90 opacity-60 h-12 p-3 ${mode === "false"? "border-gray-600" : " border-gray-200 border-r-transparent border-l-transparent"}`}>
                                 <div className="pl-5"><p>5 left</p></div>
                                 <div className="justify-self-end pr-5"><button>Clear Completed</button></div>
                         </div>
-                        <div className={`hidden justify-center items-center mx-auto h-10 bg-white rounded-sm sm:grid grid-cols-3 gap-0.5 text-gray-500 self-end opacity-60 ${mode === "false"? "dark-card" : ""}`}>
+                        <div className={`border-2 hidden justify-center items-center p-3 h-12 bg-white rounded-sm sm:grid grid-cols-3 gap-0.5 text-gray-500 mt-auto opacity-60 ${mode === "false"? "dark-card border-gray-600" : " border-gray-200 border-r-transparent border-l-transparent"}`}>
                             <div className="opacity-60">
                                 <p>5 left</p>
                             </div>
